af388aee471872a649900db68d8e5549132c9531
Tommy Boi <tomaarongrabowski@gmail.com>
2024-02-03 12:18:45 -0500

    Updated App.tsx to use global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { GlobalStyles } from "@/constants/styles/GlobalStyles/GlobalStyles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        GlobalStyles.flex_1,
+        GlobalStyles.align_center,
+        GlobalStyles.justify_center,
+      ]}
+    >
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
